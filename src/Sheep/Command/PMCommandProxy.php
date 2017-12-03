93b9a7c94a108c601039160a311f8c9e8188180b
Dogee <dogedev10@gmail.com>
2017-12-03 15:58:33 +0000

    fix console

--- src/Sheep/Command/PMCommandProxy.php
+++ src/Sheep/Command/PMCommandProxy.php
@@ -16,12 +16,4 @@ class PMCommandProxy extends PMCommand {
 		$this->command = $command;
 		parent::__construct($command->getName(), $command->getDescription(), $command->getUsage());
 	}
-
-	public function execute(CommandSender $sender, string $commandLabel, array $args) {
-		if (!($sender instanceof ConsoleCommandSender)) {
-			$sender->sendMessage("You must execute this command from the console.");
-			return;
-		}
-		$this->command->run(new PMProblem($sender), $args);
-	}
 }
